169aa90607a201d52c077884d5647c21e38f4b84
Ryanmwangi <ryannganga13325@gmail.com>
2022-03-10 12:46:41 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle"
  * Return: 0 if existed properly, non-zero otherwise
@@ -6,5 +6,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
