c7231885579a552dd19535503a9a0bbd362708d6
Ryanmwangi <99436511+Ryanmwangi@users.noreply.github.com>
2022-03-15 22:00:08 +0300

    Update main.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,1 +1,12 @@
-int _putchar(char c)
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_islower(108);
+	return (0);
+}
